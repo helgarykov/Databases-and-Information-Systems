abd440fe65623ae1aaf6541bbaaa6d85b8012fdf
Victoriast8 <42806739+Victoriast8@users.noreply.github.com>
2023-06-08 14:10:38 +0200

    updated functions

--- Project/DISProject/PostgreSQL/funs.sql
+++ Project/DISProject/PostgreSQL/funs.sql
@@ -1,5 +1,4 @@
-
--- Fun for Task Table to get LanguageId
+-- Fun for Task Table to get TranslatorId
 CREATE FUNCTION transfun(p_id integer) RETURNS integer AS $$
     SELECT COALESCE(TranslatorId, 0)
     FROM Translator_Competence AS tc
@@ -13,27 +12,20 @@ CREATE FUNCTION langfun(p_id integer) RETURNS integer AS $$
     WHERE tc.id = p_id;
 $$ LANGUAGE SQL;
 
-
 -- Fun for Task_Review Table to get TranslatorId
 CREATE OR REPLACE FUNCTION transfun1(p_id integer) 
 RETURNS integer AS $$
     SELECT COALESCE (TC.TranslatorId, 0) 
-    FROM Task_Review AS TR
-	INNER JOIN Task AS T ON TR.TaskId = T.Id
+    FROM Task AS T
 	INNER JOIN Translator_Competence AS TC ON T.TranslatorCompetenceID = TC.Id
-    WHERE TR.Id = p_id;
+    WHERE T.id = p_id;
 $$ LANGUAGE SQL;
 
 -- Fun for Task_Review Table to get LanguageId
 CREATE OR REPLACE FUNCTION langfun1(p_id integer) 
 RETURNS integer AS $$
     SELECT COALESCE (TC.LanguageId, 0) 
-    FROM Task_Review AS TR
-	INNER JOIN Task AS T ON TR.TaskId = T.Id
+	FROM Task AS T
 	INNER JOIN Translator_Competence AS TC ON T.TranslatorCompetenceID = TC.Id
-    WHERE TR.Id = p_id;
+    WHERE T.id = p_id;
 $$ LANGUAGE SQL;
-
---SELECT transfun1(5);
---SELECT langfun(40);
---SELECT transfun(1); // use paranthesis () around SELECT statement when inserting into VALUES
